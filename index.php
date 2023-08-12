aa0bc5782f5ae8ed4d51b9475ba0fb346924d935
gandiol <wos1700ous@gmail.com>
2023-08-12 23:43:51 +0200

    add style file

--- index.php
+++ index.php
@@ -29,8 +29,8 @@ $projets = $statement->fetchAll(PDO::FETCH_ASSOC);
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" integrity="sha384-PJsj/BTMqILvmcej7ulplguok8ag4xFTPryRq8xevL7eBYSmpXKcbNVuy+P0RMgq" crossorigin="anonymous">
 
-  <!-----my-css-------->
-  <link rel="stylesheet" href="app.css">
+  <!-----my-css--
+  <link rel="stylesheet" href="app.css">------>
   <title>Project CRUD</title>
 </head>
 
